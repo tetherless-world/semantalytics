2b950e0f0037423e69c99cc984b65593e281094c
John Sheehan <sheehj4@rpi.edu>
2016-05-25 11:05:31 -0400

    Added CPPData script and workflow

--- yw_play/Build.bat
+++ yw_play/Build.bat
@@ -1,5 +1,5 @@
-erase TensorExplorationPARAFAC.gv
-erase TensorExplorationPARAFACjjs.gv
-java -jar yesworkflow-0.2-SNAPSHOT-jar-with-dependencies.jar graph TensorExplorationPARAFACjjs.m
-copy TensorExplorationPARAFACjjs.gv TensorExplorationPARAFAC.gv
-dot -Tpdf TensorExplorationPARAFACjjs.gv -o TensorExplorationPARAFAC.pdf
+erase CPPData.gv
+erase CPPDatajjs.gv
+java -jar yesworkflow-0.2-SNAPSHOT-jar-with-dependencies.jar graph CPPDatajjs.m
+copy CPPDatajjs.gv CPPData.gv
+dot -Tpdf CPPData.gv -o CPPData.pdf
